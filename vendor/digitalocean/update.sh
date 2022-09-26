64c85f9fa17781d55f6f6422d90607b0d8fa7583
Jens Langhammer <jens.langhammer@beryju.org>
2022-09-26 10:07:56 +0200

    update do scripts

---
+++ vendor/digitalocean/update.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget -O 90-cleanup.sh https://raw.githubusercontent.com/digitalocean/marketplace-partners/master/scripts/90-cleanup.sh
+wget -O 99-img-check.sh https://raw.githubusercontent.com/digitalocean/marketplace-partners/master/scripts/99-img-check.sh
